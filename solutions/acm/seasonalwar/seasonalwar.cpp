ed2534b02bd3052f639a587049bb322bbfa9865f
raghavendran n <n.raghavendran@gmail.com>
2013-02-16 00:13:32 +0530

    The neighbours that need to be checked are 8. This I got from minesweeper code. There was a hardcoded value of 4 in the neighbours check which is removed to a #define. Submitted, got accepted.

--- solutions/acm/seasonalwar/seasonalwar.cpp
+++ solutions/acm/seasonalwar/seasonalwar.cpp
@@ -1,4 +1,5 @@
 #define MAXDIMENSION 27
+#define NUMNEIGHBOURS 8
 #include <iostream>
 #include <cmath>
 #include <string.h>
@@ -13,7 +14,7 @@ int cellTypes[MAXDIMENSION][MAXDIMENSION];
 class SeasonalWar {
 	int N;
 	int nEagles;
-	int imageNum;
+	unsigned long long imageNum;
 	
 private:
 	void init() {
@@ -57,13 +58,29 @@ public:
 		}
 	}
 private:
+	void printBitmap() {
+		for(int i = 1; i <= N; ++i) {
+			for(int j = 1; j <= N; ++j) {
+				cout << bitmap[i][j];
+			}
+			cout << endl;
+		}
+		cout << "State:" << endl;
+		for(int i = 1; i <= N; ++i) {
+			for(int j = 1; j <= N; ++j) {
+				cout << cellTypes[i][j];
+			}
+			cout << endl;
+		}
+	}
 	void findEagles() {
 		for(int i = 1; i <= N; ++i) {
 				for(int j = 1; j <= N; ++j) {
 				//cout << "cellTypes[" << i << "][" << j << "] is= " << cellTypes[i][j] << endl;
 				if (bitmap[i][j] == 1 && cellTypes[i][j] == NORMAL) {
+					//printBitmap();
 					++nEagles;
-					//	//cout << i << " " << j << " " << bitmap[i][j] << "is a part of EAGLE... ";
+					//cout << i << " " << j << " " << bitmap[i][j] << "is a part of EAGLE... " << nEagles << endl;
 					spanEagleUsingDFS(i, j);
 				}
 			}
@@ -86,32 +103,59 @@ private:
 	}
 
 	void initNeighbourRowsCols(int* pRows, int* pCols, int i, int j) {
+		//int left	= j - 1;
+		//int top	= i - 1;
+		//int right	= j + 1;
+		//int bottom= i + 1;
+		/*bitmap[top][left]	
+		bitmap[top][col]		
+		bitmap[top][right]	
+		bitmap[row][right]	
+		bitmap[bottom][right]
+		bitmap[bottom][col]	
+		bitmap[bottom][left]	
+		bitmap[row][left]*/	
+
 		pRows[0] = i - 1;
-		pRows[1] = i + 1;
-		pRows[2] = i;
+		pRows[1] = i - 1;
+		pRows[2] = i - 1 ;
 		pRows[3] = i;
-		pCols[0] = j;
+		pRows[4] = i + 1;
+		pRows[5] = i + 1;
+		pRows[6] = i + 1;
+		pRows[7] = i;
+
+		pCols[0] = j - 1;
 		pCols[1] = j;
-		pCols[2] = j - 1;
+		pCols[2] = j + 1;
 		pCols[3] = j + 1;
+		pCols[4] = j + 1;
+		pCols[5] = j;
+		pCols[6] = j - 1;
+		pCols[7] = j - 1;
 	}
 
 	void spanEagleUsingDFS(int i, int j) {
-		if(areIndicesValid(i, j) == false)
+		if(areIndicesValid(i, j) == false) {
+			//cout << i << " " << j << "indices are invalid." << endl;
 			return;
+		}
 		
-		int* pRows = new int[4];
-		int* pCols = new int[4];
+		int* pRows = new int[NUMNEIGHBOURS];
+		int* pCols = new int[NUMNEIGHBOURS];
 		initNeighbourRowsCols(pRows, pCols, i, j);
 
 		if(cellTypes[i][j] == NORMAL) {
 			cellTypes[i][j] = EAGLE;
-			for(int m = 0; m < 4; ++m) {
+			//cout << i << " " << j << "cellType:" << cellTypes[i][j] << " is part of eagle:" << nEagles << endl;
+
+			//printBitmap();
+			for(int m = 0; m < NUMNEIGHBOURS; ++m) {
 				if(areIndicesValid(pRows[m], pCols[m]) == false)
 					continue;
 
 				if (bitmap[pRows[m]][pCols[m]] == 1 && cellTypes[pRows[m]][pCols[m]] == NORMAL) {
-					//cout << i << " " << j << " neighbour " << pRows[m] << " " << cols[m] << "is unlocked" << endl;
+					//cout << i << " " << j << " neighbour " << pRows[m] << " " << pCols[m] << "is part of eagle:" << nEagles << endl;
 					spanEagleUsingDFS(pRows[m], pCols[m]);
 				}
 			}
