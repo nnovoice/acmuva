6941df8e8b7e05d161fea31edb3e6906dc2e3a2f
raghavendran n <n.raghavendran@gmail.com>
2013-02-26 18:28:53 +0530

    initial version

---
+++ solutions/acm/primaryarithmetic/primaryarithmetic.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+class PrimaryArithmetic {
+public:
+	PrimaryArithmetic() { }
+	int process() {
+
+		return 0;
+	}
+	int numCarryOvers() {
+
+		return 0;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	PrimaryArithmetic digitAdder;
+	digitAdder.process();
+	return 0;
+}
