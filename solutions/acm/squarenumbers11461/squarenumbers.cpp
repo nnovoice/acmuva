fa5de8f96f90f727a5e4fc3d32a941ddb32280c5
raghavendran n <n.raghavendran@gmail.com>
2013-04-01 17:48:01 +0530

    Code seems complex to be built upon bin search. there are special cases to be take care of: 10 15 80 82 122 122

--- solutions/acm/squarenumbers11461/squarenumbers.cpp
+++ solutions/acm/squarenumbers11461/squarenumbers.cpp
@@ -1,6 +1,7 @@
 #define MAX 100000
 #define NUMSQUARENUMBERS 316
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 int getSquareIndex(int* squares, int num) {
@@ -9,6 +10,27 @@ int getSquareIndex(int* squares, int num) {
 			return i - 1;
 		}
 	}
+	return -1;
+}
+
+bool isSquareNumber(int* squares, int num) {
+	int low = 1;
+	int high = NUMSQUARENUMBERS;
+	int mid = 0;
+	while (low < high) {
+		mid = low + ((high - low) / 2);
+		if (squares[mid] == num) {
+			return true;
+		}
+		else if (squares[mid] < num) {
+			low = mid + 1;
+		}
+		else {
+			high = mid - 1;
+		}
+	}
+	
+	return false;
 }
 
 int getSquareIndexUsingBinSearch(int* squares, int num) {
@@ -30,30 +52,65 @@ int getSquareIndexUsingBinSearch(int* squares, int num) {
 			high = mid - 1;
 		}
 	}
-	if (squares[mid] > num)
+	
+	if (squares[mid] > num) {
 		return mid - 1;
-	return mid;
+	}
+	
+	return mid + 1;
+}
+
+void constructSquares(int* squares, int nMaxSquares) {
+	squares[0] = 0;
+	for (int i = 1; i <= nMaxSquares; ++i) {
+		squares[i] = i * i;
+	}
+}
+
+int getNumSquaresBetween(int* squares, int a, int b) {
+	bool isSquareNumA = isSquareNumber(squares, a);
+	bool isSquareNumB = isSquareNumber(squares, b);
+	
+	int aIndex = getSquareIndexUsingBinSearch(squares, a);
+	int bIndex = getSquareIndexUsingBinSearch(squares, b);
+	
+	int diff = aIndex - bIndex;
+	if (diff < 0)
+		diff = 0 - diff;
+
+	if (diff != 0) {
+		diff -= 1; // index 1, index 3 yields diff = 2, but # of squares b/w them is 1.
+		diff += 2;
+		/*if (isSquareNumA)
+			diff += 1; 
+		if (isSquareNumB)
+			diff += 1;*/
+	}
+	else {
+		int numDiff = a - b;
+		if (numDiff != 0) {
+			diff += 1;
+		}
+		else {
+			if (isSquareNumA || isSquareNumB)
+				diff += 1;
+		}
+	}
+	return diff;
 }
 
 int main() {
+	//freopen("C:\\data\\personal\\programming\\acm\\input_files\\squarenumbers11461\\input.txt", "r", stdin);
 	int squares[NUMSQUARENUMBERS + 1];
-	squares[0] = 0;
-	for (int i = 1; i*i <= MAX; ++i) {
-		squares[i] = i * i;
-	}
+	constructSquares (squares, NUMSQUARENUMBERS);
     
 	int a = 0;
 	int b = 0;
-	int aIndex = 0;
-	int bIndex = 0;
-	int diff = 0;
 	while (cin >> a >> b) {
-		aIndex = getSquareIndex(squares, a);
-		bIndex = getSquareIndex(squares, b);
-		diff = aIndex - bIndex;
-		if (diff < 0)
-			diff = 0 - diff;
-		cout << diff << endl;
+		if (a == 0 && b == 0) {
+			return 0;
+		}
+		cout << getNumSquaresBetween(squares, a, b) << endl;
 	}
+	return 0;
 }
-
