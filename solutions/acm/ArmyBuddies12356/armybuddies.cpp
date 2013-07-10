76a107a22994eb4fa01cc9782568b39fd508410d
raghavendran n <n.raghavendran@gmail.com>
2013-07-10 22:42:51 +0530

    bitset gives RTE (run time error)

--- solutions/acm/ArmyBuddies12356/armybuddies.cpp
+++ solutions/acm/ArmyBuddies12356/armybuddies.cpp
@@ -1,12 +1,12 @@
 /* UVa 12356 - ArmyBuddies*/
 #define MAXSOLDIERS 100001
 #include <iostream>
-#include <string.h>
+#include <bitset>
 using namespace std;
 
 int main()
 {
-	int soldiers[MAXSOLDIERS] = {0};
+	bitset <MAXSOLDIERS> soldiers;
 	int nSoldiers = 0;
 	int nLossReports = 0;
 	int leftSoldier = 0;
@@ -16,43 +16,31 @@ int main()
 		if (nSoldiers == 0 && nLossReports == 0) 
 			break;
 
-		// memset sets each byte to the value passed to it
-		// so, it is not equivalent to 2, but 0x02020202
-		// memset (soldiers, 2, sizeof(int) * MAXSOLDIERS);
-
-		// set to 0 means everyone is alive. set to 1 means there is a loss.
-		memset (soldiers, 0, sizeof(int) * MAXSOLDIERS);
-		soldiers[0] = 1;
-		soldiers[nSoldiers + 1] = 1;
+		soldiers.set();
+		soldiers.flip(0);
+		soldiers.flip(nSoldiers + 1);
 
 		for (int i = 0; i < nLossReports; ++i) {
-			/*cout << "Debug: ";
-			for (int k = 1; k <= nSoldiers; ++k) {
-				cout << soldiers[k] << " ";
-			}
-			cout << endl;*/
 			cin >> leftSoldier >> rightSoldier;
-			//cout << "Debug: " << leftSoldier << " " << rightSoldier << endl;
 
 			// mark the losses in range [left, right]
 			for (int j = leftSoldier; j <= rightSoldier; ++j)
-				soldiers[j] = 1;
+				soldiers.flip(j);
 
 			int leftBuddy = leftSoldier - 1;
 			for (; leftBuddy > 0; --leftBuddy)
-				if (soldiers[leftBuddy] == 0)
+				if (soldiers.test(leftBuddy) == true)
 					break;
-			//cout << "Debug: " << "Left buddy = " << leftBuddy << endl;
+			
 			(leftBuddy > 0) ? (cout << leftBuddy) : (cout << '*');
 			cout << ' ';
 
 			int rightBuddy = rightSoldier + 1;
 			for (; rightBuddy <= nSoldiers; ++rightBuddy)
-				if (soldiers[rightBuddy] == 0)
+				if (soldiers.test(rightBuddy) == true)
 					break;
-			//cout << "Debug: " << "Right buddy = " << rightBuddy << endl;
 
-			(soldiers[rightBuddy] == 0) ? (cout << rightBuddy) : (cout << '*');
+			(soldiers.test(rightBuddy) == true) ? (cout << rightBuddy) : (cout << '*');
 			cout << endl;
 		}
 		cout << '-' << endl;
