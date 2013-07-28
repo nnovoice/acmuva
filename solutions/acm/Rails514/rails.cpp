75ac45e38b8c9e0f4f8009eeb2b69d2438611525
raghavendran n <n.raghavendran@gmail.com>
2013-07-28 10:15:16 +0530

    Getting WA for some reason. Need to change a bit.

--- solutions/acm/Rails514/rails.cpp
+++ solutions/acm/Rails514/rails.cpp
@@ -10,65 +10,71 @@ int main()
 {
     int nCoaches = 0;
     int coach = 0;
-    //int coachesInOrder[MAXCOACHES] = {0};
     int coachNum = 1;
     int coachToMove = 0;
     bool haveCoachesToProcess = true;
 
-//    for (int i = 0; i < MAXCOACHES; ++i) {
-//        coachesInOrder[i] = i;
-//    }
-
     queue<int> expectedOrder;
     stack<int> stationCoaches;
 
-    freopen ("C:\\data\\personal\\programming\\acm\\input_files\\Rails514\\input.txt", "r", stdin);
+    //freopen ("C:\\data\\personal\\programming\\acm\\input_files\\Rails514\\input.txt", "r", stdin);
 
     while (cin >> nCoaches) {
         if (nCoaches == 0) break;
 
-        haveCoachesToProcess = true;
-        coachNum = 1;
+        if (haveCoachesToProcess == false)
+            cout << endl;
 
-        for (int i = 0; i < nCoaches; ++i) {
-            cin >> coach;
+        while (1) {
+            haveCoachesToProcess = true;
+            coachNum = 1;
 
-            if (coach == 0) {
-                haveCoachesToProcess = false;
-                cout << endl;
-                break;
+            while (expectedOrder.empty() == false)
+                expectedOrder.pop();
+
+            while (stationCoaches.empty() == false)
+                stationCoaches.pop();
+
+            for (int i = 0; i < nCoaches; ++i) {
+                cin >> coach;
+
+                if (coach == 0) {
+                    haveCoachesToProcess = false;
+                    //cout << endl;
+                    break;
+                }
+
+                expectedOrder.push(coach);
             }
 
-            expectedOrder.push(coach);
-        }
+            if (haveCoachesToProcess == false)
+                break;
 
-        if (haveCoachesToProcess == false)
-            continue;
+            while (expectedOrder.empty() == false) {
+                coachToMove = expectedOrder.front();
+                expectedOrder.pop();
 
-        while (expectedOrder.empty() == false) {
-            coachToMove = expectedOrder.front();
-            expectedOrder.pop();
+                for (; coachNum <= coachToMove; ++coachNum) {
+                    stationCoaches.push(coachNum);
+                }
 
-            for (; coachNum <= coachToMove; ++coachNum) {
-                stationCoaches.push(coachNum);
+                if (stationCoaches.empty() == false && stationCoaches.top() == coachToMove) {
+                    stationCoaches.pop();
+                }
+                else {
+                    break;
+                }
             }
 
             if (stationCoaches.empty() == true) {
-                cout << "No" << endl;
-                break;
-            }
-
-            if (stationCoaches.top() == coachToMove) {
-                stationCoaches.pop();
+                if (expectedOrder.empty() == true)
+                    cout << "Yes" << endl;
+                else
+                    cout << "No" << endl;
             }
             else {
                 cout << "No" << endl;
-                break;
             }
-        }
-
-        if (expectedOrder.empty() == true && coachNum == nCoaches) {
-                cout << "Yes" << endl;
         }
     }
 }
