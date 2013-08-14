5b2274ad7e14b5bcb4741c41979526d028e96c39
raghavendran n <n.raghavendran@gmail.com>
2013-08-14 17:26:26 +0530

    Getting WA because it does not handle cars arriving at the same time on the same bank

--- solutions/acm/FerryLoadingIII10901/FerryLoadingIII10901.cpp
+++ solutions/acm/FerryLoadingIII10901/FerryLoadingIII10901.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <map>
+#include <cstdio>
 
 using namespace std;
 
@@ -9,11 +10,15 @@ void MoveToOtherBank (bank& ferryBank, bank currentBank, bank otherBank,
                       queue<unsigned int>& otherBankCarsQueue, unsigned int& currentFerryTime, unsigned int& nFerryTravelTime)
 {
     ferryBank = otherBank;
-    // wait until the car arrives
-    currentFerryTime = otherBankCarsQueue.front();
-    // travel to pick it up
+
+	// wait until the car arrives
+	if (currentFerryTime < otherBankCarsQueue.front()) // LEARNING: i had blindly assigned the current ferry time to the front
+		currentFerryTime = otherBankCarsQueue.front();
+
+	// travel to pick it up
     currentFerryTime += nFerryTravelTime;
 }
+
 void StayIntheCurrentBank(bank& ferryBank, bank currentBank, bank otherBank,
                       queue<unsigned int>& currentBankCarsQueue, unsigned int& currentFerryTime, unsigned int& nFerryTravelTime)
 {
@@ -25,36 +30,39 @@ void StayOrMoveToOtherBank(bank& ferryBank, bank currentBank, bank otherBank,
                            queue<unsigned int>& currentBankCarsQueue, queue<unsigned int>& otherBankCarsQueue,
                            unsigned int& currentFerryTime, unsigned int& nFerryTravelTime)
 {
+    //cout << "Debug: " << "StayOrMoveToOtherBank: " << "ferry bank= " << ferryBank << " current ferry time= " << currentFerryTime << endl;
     if (currentBankCarsQueue.empty() == false && otherBankCarsQueue.empty() == false) {
-        if (currentBankCarsQueue.front() < otherBankCarsQueue.front()) {
+        if (currentBankCarsQueue.front() <= otherBankCarsQueue.front()) { // LEARNING: subtle bug here: instead of <=, i had < which caused trouble
             StayIntheCurrentBank(ferryBank, currentBank, otherBank, currentBankCarsQueue, currentFerryTime, nFerryTravelTime);
         }
         else {
             MoveToOtherBank (ferryBank, currentBank, otherBank, otherBankCarsQueue, currentFerryTime, nFerryTravelTime);
         }
     }
-    else if (otherBankCarsQueue.empty() == false) {
-        MoveToOtherBank (ferryBank, currentBank, otherBank, otherBankCarsQueue, currentFerryTime, nFerryTravelTime);
-    }
     else if (currentBankCarsQueue.empty() == false) {
         StayIntheCurrentBank(ferryBank, currentBank, otherBank, currentBankCarsQueue, currentFerryTime, nFerryTravelTime);
     }
-
+    else if (otherBankCarsQueue.empty() == false) {
+        MoveToOtherBank (ferryBank, currentBank, otherBank, otherBankCarsQueue, currentFerryTime, nFerryTravelTime);
+    }
     //cout << "Debug: " << "StayOrMoveToOtherBank: " << "ferry bank= " << ferryBank << " current ferry time= " << currentFerryTime << endl;
 }
 
-unsigned int LoadAndMoveCars(queue<unsigned int>& carsQueue, map<unsigned int, unsigned int>& carTimesMap,
-                    unsigned int& currentFerryTime, unsigned int& nFerryTravelTime,
-                    unsigned int& nCars)
+unsigned int LoadAndMoveCars(bank& ferryBank, queue<unsigned int>& carsQueue,
+                            map<unsigned int, unsigned int>& carIDsMap, map<unsigned int, unsigned int>& carTimesMap,
+                            unsigned int& currentFerryTime, unsigned int& nFerryTravelTime,
+                            unsigned int& nCars)
 {
     unsigned int nCarsLoaded = 0;
+    unsigned int carID = 0;
 
     while (carsQueue.empty() == false) {
         if (carsQueue.front() <= currentFerryTime) {
             if (nCarsLoaded < nCars) { // LEARNING: subtle bug here: instead of <, i had <= which caused trouble
                 ++nCarsLoaded;
-                carTimesMap[carsQueue.front()] = currentFerryTime + nFerryTravelTime;
-                //cout << "Debug: " << "car arrival= " << carsQueue.front() << " drop time= " << (currentFerryTime + nFerryTravelTime) << endl;
+                carID = carTimesMap[carsQueue.front()];
+                carIDsMap[carID] = (currentFerryTime + nFerryTravelTime);
+                //cout << "Debug: " << "car arrival= " << carsQueue.front() << "carID= " << carID << " drop time= " << (currentFerryTime + nFerryTravelTime) << " from the map= " << carIDsMap[carID] << endl;
                 carsQueue.pop();
             }
             else {
@@ -79,7 +87,11 @@ int main()
     queue<unsigned int> leftBankCarsQueue;
     queue<unsigned int> rightBankCarsQueue;
     queue<unsigned int> allCarsQueue;
-    map<unsigned int, unsigned int> carTimesMap;
+    map<unsigned int, unsigned int> leftBankCarTimesMap;
+    map<unsigned int, unsigned int> leftBankCarIDsMap;
+    map<unsigned int, unsigned int> rightBankCarTimesMap;
+    map<unsigned int, unsigned int> rightBankCarIDsMap;
+	unsigned int carID = 0;
     bank ferryBank = leftbank;
 
     unsigned int nFerryTravelTime = 0;
@@ -92,29 +104,39 @@ int main()
     unsigned int nLeftCarsLoaded = 0;
     unsigned int nRightCarsLoaded = 0;
 
+    //freopen ("C:\\data\\personal\\programming\\acm\\input_files\\FerryLoadingIII10901\\ferry3.txt", "r", stdin);
+    //C:\data\personal\programming\acm\input_files\FerryLoadingIII10901
     cin >> nCases;
     for (unsigned int i = 0; i < nCases; ++i) {
         cin >> nCars >> nFerryTravelTime >> nLines;
-       // //cout << "Debug: " << nCars << " " << nFerryTravelTime << " " << nLines << endl;
+        //cout << "Debug: " << nCars << " " << nFerryTravelTime << " " << nLines << endl;
 
-        carTimesMap.erase (carTimesMap.begin(), carTimesMap.end());
+        leftBankCarTimesMap = map<unsigned int, unsigned int>();
+        leftBankCarIDsMap = map<unsigned int, unsigned int>();
+        rightBankCarTimesMap = map<unsigned int, unsigned int>();
+        rightBankCarIDsMap = map<unsigned int, unsigned int>();
         leftBankCarsQueue = queue<unsigned int>();
         rightBankCarsQueue = queue<unsigned int>();
         allCarsQueue = queue<unsigned int>();
         ferryBank = leftbank;
         currentFerryTime = 0;
+		carID = 0;
 
         for (unsigned int j = 0; j < nLines; ++j) {
             cin >> nCarArrivalTime >> side;
-           // //cout << "Debug: " << nCarArrivalTime << " " << side << endl;
+            //cout << "Debug: " << nCarArrivalTime << " " << side << endl;
             if (side[0] == 'l' && side[1] == 'e' && side[2] == 'f' && side[3] == 't') {
                 leftBankCarsQueue.push (nCarArrivalTime);
+				leftBankCarTimesMap[nCarArrivalTime] = carID;
+				leftBankCarIDsMap[carID] = nCarArrivalTime;
             }
             else {
                 rightBankCarsQueue.push (nCarArrivalTime);
+                rightBankCarTimesMap[nCarArrivalTime] = carID;
+                rightBankCarIDsMap[carID] = nCarArrivalTime;
             }
-
-            allCarsQueue.push(nCarArrivalTime);
+            allCarsQueue.push(carID);
+            ++carID;
         }
 
         while (1) {
@@ -125,7 +147,8 @@ int main()
 
             nLeftCarsLoaded = 0;
             if (ferryBank == leftbank) {
-                nLeftCarsLoaded = LoadAndMoveCars(leftBankCarsQueue, carTimesMap,
+                nLeftCarsLoaded = LoadAndMoveCars(ferryBank, leftBankCarsQueue,
+                                                  leftBankCarIDsMap, leftBankCarTimesMap,
                                                   currentFerryTime, nFerryTravelTime,
                                                   nCars);
                 //cout << "Debug: " << "leftbank cars loaded= " << nLeftCarsLoaded << endl;
@@ -142,7 +165,8 @@ int main()
 
             nRightCarsLoaded = 0;
             if (ferryBank == rightbank) {
-                nRightCarsLoaded = LoadAndMoveCars(rightBankCarsQueue, carTimesMap,
+                nRightCarsLoaded = LoadAndMoveCars(ferryBank, rightBankCarsQueue,
+                                                   rightBankCarIDsMap, rightBankCarTimesMap,
                                                    currentFerryTime, nFerryTravelTime,
                                                    nCars);
                 //cout << "Debug: " << "rightbank cars loaded= " << nRightCarsLoaded << endl;
@@ -159,7 +183,13 @@ int main()
         }
 
         while (allCarsQueue.empty() == false) {
-            cout << carTimesMap[allCarsQueue.front()] << endl;
+            //cout << "Debug: " << "CarID= " << allCarsQueue.front();
+            if (leftBankCarIDsMap.find(allCarsQueue.front()) != leftBankCarIDsMap.end())
+                cout << leftBankCarIDsMap[allCarsQueue.front()];
+            else if(rightBankCarIDsMap.find(allCarsQueue.front()) != rightBankCarIDsMap.end())
+                cout << rightBankCarIDsMap[allCarsQueue.front()];
+
+            cout << endl;
             allCarsQueue.pop();
         }
 
