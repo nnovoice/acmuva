6e392ed891a274e756a34d9e43aba14e8c7b69e6
raghavendran n <n.raghavendran@gmail.com>
2013-08-15 17:51:36 +0530

    Found an interesting explanation in the book 'Algorithms for Interviews' for just this same problem except that it was about doors.

--- solutions/acm/LightMoreLight10110/LightMoreLight10110.cpp
+++ solutions/acm/LightMoreLight10110/LightMoreLight10110.cpp
@@ -1,30 +1,40 @@
+/* UVa 10110 - Light More Light */
 #include <iostream>
-#include <bitset>
+#include <vector>
+#include <climits>
+#include <algorithm>
 
 using namespace std;
 
-const unsigned long int MAXBULBS = 4294967295;
-bitset<MAXBULBS> switchStates;
-
 int main()
 {
-    unsigned int num = 0;
-    switchStates.reset();
+    vector<unsigned long long int> squares;
+    unsigned long long int uIntMaxValue = (unsigned long long int) UINT_MAX;
+    for (unsigned long long int i = 1; (i * i) <= uIntMaxValue; ++i) {
+        squares.push_back(i * i);
+    }
+
+//    for (unsigned int i = 0; i < squares.size(); ++i)
+//        cout << i << " " << squares[i] << endl;
+
+    unsigned long int num = 0;
+    bool switchState = false;
     while (cin >> num) {
         if (num == 0)
             break;
 
-        switchStates.reset(num);
-        for (unsigned long i = 1; i <= num; ++i) {
-            for (unsigned long j = 1; j <= num; j += i) {
-                if ((j % i) == 0)
-                    switchStates.flip(j);
-            }
-        }
-        if (switchStates.test(num) == true)
+        switchState = binary_search(squares.begin(), squares.end(), num);
+
+        if (switchState == true)
             cout << "yes" << endl;
         else
             cout << "no" << endl;
     }
+
+//    cout << "uint_max= " << UINT_MAX << endl;
+//    cout << "long_max= " << LONG_MAX << endl;
+//    cout << "long long max= " << LONG_LONG_MAX << endl;
+//    cout << "unsigned long long max= " << ULONG_LONG_MAX << endl;
+
     return 0;
 }
