1c3c55fa38b628086f530719167ed0a49f4b3883
raghavendran n <n.raghavendran@gmail.com>
2013-08-18 00:50:26 +0530

    v1.0...is wrong because the values are overflowing! need a much better implementation

---
+++ solutions/acm/Combinations369/Combinations369.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long int pascalTriangle[103][103] = {};
+    unsigned int N = 0;
+    unsigned int K = 0;
+
+    unsigned int i = 0;
+    unsigned int j = 0;
+
+    pascalTriangle[1][1] = 1;
+    pascalTriangle[2][1] = 1; pascalTriangle[2][2] = 1;
+
+    for (i = 3; i <= 102; ++i) {
+        pascalTriangle[i][1] = 1; //cout << pascalTriangle[i][1] << ", ";
+        for (j = 2; j < i; ++j) {
+            pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+            //cout << pascalTriangle[i][j] << ", ";
+        }
+        pascalTriangle[i][j] = 1;
+        //cout << pascalTriangle[i][j] << " " << endl;
+    }
+
+    while (cin >> N >> K) {
+        if (N == 0 && K == 0)
+            break;
+
+        cout << N << " things taken " << K << " at a time is " << pascalTriangle[N + 1][K + 1] << " exactly." << endl;
+    }
+
+//    cout << "100 choose 6= " << pascalTriangle[101][7] << endl;
+//    cout << "20 choose 5= " << pascalTriangle[21][6] << endl;
+//    cout << "18 choose 6= " << pascalTriangle[19][7] << endl;
+    return 0;
+}
