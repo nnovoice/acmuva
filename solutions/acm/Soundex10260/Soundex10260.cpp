591c805394b476546cbdc61e247b606b5ddbe679
raghavendran n <n.raghavendran@gmail.com>
2013-09-18 06:55:18 +0530

    AC :-) Problems: 1. lastChar was int 2. lastChat was not initialized to ' '  in every test case.
    Removed the output array, idx, check for idx > 0. Kept it simple.

--- solutions/acm/Soundex10260/Soundex10260.cpp
+++ solutions/acm/Soundex10260/Soundex10260.cpp
@@ -1,17 +1,13 @@
 /* UVa 10260 - Soundex*/
 #include <stdio.h>
-#include <string.h>
 #define MAXCHARS 20
 
 int main()
 {
     char input[MAXCHARS] = {'\0'};
-    char output[MAXCHARS] = {'\0'};
-    int idx = 0;
-    int lastChar = ' ';
+    char lastChar = ' ';
     while (scanf("%s", input) != EOF) {
-        memset(output, '\0', sizeof(output));
-        idx = 0;
+        lastChar = ' ';
         for (int i = 0; input[i] != '\0'; ++i) {
             if (lastChar != input[i]) {
                 switch(input[i]) {
@@ -30,8 +26,7 @@ int main()
                 case 'P':
                 case 'V':
                     if (lastChar != 'B' && lastChar != 'F' && lastChar != 'P' && lastChar != 'V')
-                        output[idx++] = '1';
-                        //printf ("1");
+                        printf ("1");
                     break;
                 //2 represents C, G, J, K, Q, S, X,  or Z
                 case 'C':
@@ -44,39 +39,32 @@ int main()
                 case 'Z':
                     if (lastChar != 'C' && lastChar != 'G' && lastChar != 'J' && lastChar != 'K' &&
                         lastChar != 'Q' && lastChar != 'S' && lastChar != 'X' && lastChar != 'Z')
-                        output[idx++] = '2';
-                        //printf("2");
+                        printf("2");
                     break;
                 //3 represents D or T
                 case 'D':
                 case 'T':
                     if (lastChar != 'D' && lastChar != 'T')
-                        output[idx++] = '3';
-                        //printf ("3");
+                        printf ("3");
                     break;
                 // 4 represents L
                 case 'L':
-                    output[idx++] = '4';
-                    //printf ("4");
+                    printf ("4");
                     break;
                 //5 represents M or N
                 case 'M':
                 case 'N':
                     if (lastChar != 'M' && lastChar != 'N')
-                        output[idx++] = '5';
-                        //printf ("5");
+                        printf ("5");
                     break;
                 // 6 represents R
                 case 'R':
-                    output[idx++] = '6';
-                    //printf ("6");
+                    printf ("6");
                     break;
                 }
             }
             lastChar = input[i];
         }
-        if (idx > 0)
-            printf("%s", output);
 
         printf("\n");
     }
