0f943d3aaba4984ebc65a1a5ad324c75f4d49464
raghavendran n <n.raghavendran@gmail.com>
2013-09-20 12:11:55 +0530

    back to my version with 0.062s

--- solutions/acm/3n+1/3n+1_100.cpp
+++ solutions/acm/3n+1/3n+1_100.cpp
@@ -39,21 +39,12 @@ unsigned int GetCycleCount (unsigned int n)
     return cycles[n];
 }
 
-unsigned collatz_aux(unsigned n, unsigned count)
-{
-      if (n == 1)
-           return count;
-      else if (n % 2 == 0)
-           return collatz_aux(n / 2, count + 1);
-      else
-           return collatz_aux(3 * n + 1, count + 1);
-}
 int main()
 {
   unsigned int maxCycleCount = 0;
   cycles[1] = 1;
   for (unsigned int i = 2; i <= MAXNUMBERS; ++i) {
-    cycles[i] = collatz_aux(i, 0) + 1;
+    cycles[i] = GetCycleCount(i);
     //printf("%u,%u\n", i, cycles[i]);
   }
 
