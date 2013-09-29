1548c2bdc4e485fb33abcdc1ffcc0ff5460755c6
raghavendran n <n.raghavendran@gmail.com>
2013-09-30 02:29:40 +0530

    WA after 2 submits

---
+++ solutions/acm/BeatTheSpread10812/BeatTheSpread10812.cpp
@@ -0,0 +1,39 @@
+/* UVa 10812 - Beat The Spread*/
+#include <stdio.h>
+
+using namespace std;
+
+typedef unsigned long int ULI;
+
+int main()
+{
+    int nCases = 0;
+    ULI scoreA = 0;
+    ULI scoreB = 0;
+    ULI scoresTotal = 0;
+    ULI scoresDiff = 0;
+    scanf("%d", &nCases);
+    for (int i = 0; i < nCases; ++i)
+    {
+        scanf("%lu %lu", &scoresTotal, &scoresDiff);
+        // x + y = scoresTotal
+        // x - y = scoresDiff
+        // 2x = scoresTotal + scoresDiff
+        // x = (scoresTotal + scoresDiff) / 2
+        // y = scoresTotal - x
+        if (scoresTotal <= scoresDiff) {
+            printf("impossible\n");
+        }
+        else {
+            scoreA = (scoresTotal + scoresDiff) / 2;
+            scoreB = scoresTotal - scoreA;
+            if (((scoreA + scoreB) != scoresTotal) && ((scoreA - scoreB) != scoresDiff)) {
+                printf("impossible\n");
+            }
+            else {
+                printf("%lu %lu\n", scoreA, scoreB);
+            }
+        }
+    }
+    return 0;
+}
