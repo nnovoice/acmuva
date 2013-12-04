abcc35476af0b3effac9652503b5155dfa54472e
n.raghavendran <n.raghavendran@gmail.com>
2013-12-04 22:26:08 +0530

    Removed the commented line with break

--- solutions/acm/TestingTheCATCHER231/TestingTheCATCHER231.cpp
+++ solutions/acm/TestingTheCATCHER231/TestingTheCATCHER231.cpp
@@ -19,7 +19,6 @@ int GetMissilesIntercepted(int nMissiles)
                         nMaxMissilesIntercepted =  curMissilesIntercepted;
                     }
                 }
-                //break;
             }
         }
     }
