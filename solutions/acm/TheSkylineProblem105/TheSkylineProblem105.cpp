63eda4165534ab3a5bdfec6d95b1ad3efe0f2d40
n.raghavendran <n.raghavendran@gmail.com>
2014-01-09 22:18:25 +0530

    Submission with ID 12950114: Presentation Error :-)

--- solutions/acm/TheSkylineProblem105/TheSkylineProblem105.cpp
+++ solutions/acm/TheSkylineProblem105/TheSkylineProblem105.cpp
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
-const int MAXBUILDINGSCOORDINATE = 10005;
-const int MAXZOOMEDBUILDINGS = MAXBUILDINGSCOORDINATE * 5;
+const int ZOOMFACTOR = 5;
+const int MAXBUILDINGSCOORDINATE = 10010;
+const int MAXZOOMEDBUILDINGS = MAXBUILDINGSCOORDINATE * ZOOMFACTOR;
 int skyline[MAXZOOMEDBUILDINGS][2];
 const int top = 1;
 const int bot = 0;
 
 void PrintBuildings(int leftmost, int rightmost) {
-    int left = leftmost * 5;
-    int right = rightmost * 5;
+    int left = leftmost * ZOOMFACTOR;
+    int right = rightmost * ZOOMFACTOR;
     for (int i = left; i <= right; ++i) {
-        if ((i % 5) == 0)
+        if ((i % ZOOMFACTOR) == 0)
             printf("%5d", skyline[i][top]);
     }
     printf("\n");
     for (int i = left; i <= right; ++i) {
-        if ((i % 5) == 0)
+        if ((i % ZOOMFACTOR) == 0)
             printf("%5d", skyline[i][bot]);
     }
     printf("\n");
     for (int i = left; i <= right; ++i) {
-        if ((i % 5) == 0)
-            printf("%5d", (i/5));
+        if ((i % ZOOMFACTOR) == 0)
+            printf("%5d", (i/ZOOMFACTOR));
     }
     printf("\n");
 }
 
 void PrintZoomedBuildings(int leftmost, int rightmost) {
-    int left = leftmost * 5;
-    int right = rightmost * 5;
+    int left  = leftmost  * ZOOMFACTOR;
+    int right = rightmost * ZOOMFACTOR;
     printf("ZoomedLeft=%4d ZoomedRight=%4d\n", left, right);
     for (int i = left; i <= right; ++i) {
         printf("%4d", skyline[i][top]);
@@ -48,39 +49,31 @@ void PrintZoomedBuildings(int leftmost, int rightmost) {
 /// That will help in determining the common skyline connection between buildings which is
 /// otherwise difficult to find out :)
 void TraceSkyline(int leftmost, int rightmost) {
-    int zoomedLeft = leftmost * 5;
-    int zoomedRight = rightmost * 5;
+    int zoomedLeft  = leftmost  * ZOOMFACTOR;
+    int zoomedRight = rightmost * ZOOMFACTOR;
     int x = zoomedLeft;
-    int nextX = 0;
-    int prevX = 0;
-
-    //printf("Debug: ZoomedLeft=%4d ZoomedRight=%4d\n", zoomedLeft, zoomedRight);
+    int nextX = 0, prevX = 0;
 
     while (x <= zoomedRight) {
         /// travel to the first non-zero height building
-        while (skyline[x][top] == 0) ++x;
+        while (skyline[x][top] == 0 && x <= zoomedRight) {
+            ++x;
+        }
 
         if (x > zoomedRight) break;
-        //printf("Debug: %d %d\n", x, skyline[x][top]);
 
         prevX = x - 1;
-        if (x <= zoomedRight) {
-            //printf("Debug: inside prevX %d %d\n", x, skyline[x][top]);
-            if (skyline[prevX][top] == 0)
-                printf ("%d %d ", x/5, skyline[x][top]);
-            //x += 1;
-        }
+        if (skyline[prevX][top] == 0)
+            printf ("%d %d ", x/ZOOMFACTOR, skyline[x][top]);
 
         nextX = x + 1;
         if (skyline[x][top] != skyline[nextX][top]) {
-            //printf("Debug: inside NOTEQ nextX=%d %d\n", nextX, skyline[nextX][top]);
-            printf ("%d %d ", nextX/5, skyline[nextX][top]);
+            printf ("%d %d ", nextX/ZOOMFACTOR, skyline[nextX][top]);
             x += 2;
         }
         else {
             x += 1;
         }
-        //printf("Debug: near while loop end: %d %d\n", x, skyline[x][top]);
     }
 
     printf("\n");
@@ -97,8 +90,8 @@ int main()
     while (scanf("%d %d %d", &left, &height, &right) != EOF) {
         if (buildingNum == 0) leftmost = left;
         ++buildingNum;
-        zoomedInLeft  = left * 5;
-        zoomedInRight = right * 5;
+        zoomedInLeft  = left  * ZOOMFACTOR;
+        zoomedInRight = right * ZOOMFACTOR;
         for (int i = zoomedInLeft; i <= zoomedInRight; ++i) {
             if (skyline[i][top] < height) {
                 skyline[i][bot] = skyline[i][top];
@@ -111,8 +104,8 @@ int main()
         }
     }
     rightmost = right + 1; // to find the end
-    //PrintBuildings(leftmost, rightmost);
-    //PrintZoomedBuildings(leftmost, rightmost);
+
     TraceSkyline(leftmost, rightmost);
+
     return 0;
 }
