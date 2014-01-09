92ca580b6c34b6e983a779256b83f99cd86200af
n.raghavendran <n.raghavendran@gmail.com>
2014-01-09 20:07:39 +0530

    Finally got this wonderful logic: store a zoomed in version of each building. Each building is stored in a 5 times zoomed in size. So, if a building is (1 11 5) it will be stored as (5 11 25) which means start at 5, end at 25 with height as 11. Tracing the skyline using the above is quite simple :-) Go until a non-zero height building is found. Compare height at x and x + 1:     If we have a difference, print x, height(x)     else increment x Took me a couple of months, constant thinking and a whiteboard to get here. Now, for some more tests before submit.

--- solutions/acm/TheSkylineProblem105/TheSkylineProblem105.cpp
+++ solutions/acm/TheSkylineProblem105/TheSkylineProblem105.cpp
@@ -1,152 +1,114 @@
 #include <stdio.h>
-#include <vector>
-#include <algorithm>
-using namespace std;
 
-const int MAXBUILDINGS = 5002;
-int skyline[MAXBUILDINGS];
-int buildingHeights[MAXBUILDINGS];
-vector<int> buildingIDs[MAXBUILDINGS];
+const int MAXBUILDINGS = 5005;
+const int MAXZOOMEDBUILDINGS = MAXBUILDINGS * 5;
+int skyline[MAXZOOMEDBUILDINGS][2];
+const int top = 1;
+const int bot = 0;
 
-void PrintBuildings(int leftmost, int rightmost)
-{
-    vector<int>* pBuildingIDsList = 0;
-    vector<int>::iterator li;
-    vector<int>::iterator le;
-    for (int i = leftmost; i <= rightmost; ++i) {
-        printf("%5d", skyline[i]);
+void PrintBuildings(int leftmost, int rightmost) {
+    int left = leftmost * 5;
+    int right = rightmost * 5;
+    for (int i = left; i <= right; ++i) {
+        if ((i % 5) == 0)
+            printf("%5d", skyline[i][top]);
     }
     printf("\n");
-    printf("Building IDs:\n");
-    for (int i = leftmost; i <= rightmost; ++i) {
-        pBuildingIDsList = &buildingIDs[i];
-        printf("Height=%5d: {IDs=", skyline[i]);
-
-        li = pBuildingIDsList->begin();
-        le = pBuildingIDsList->end();
-        for(; li != le; ++li) {
-            printf("%d,", *li);
-        }
-        printf("}\n");
+    for (int i = left; i <= right; ++i) {
+        if ((i % 5) == 0)
+            printf("%5d", skyline[i][bot]);
+    }
+    printf("\n");
+    for (int i = left; i <= right; ++i) {
+        if ((i % 5) == 0)
+            printf("%5d", (i/5));
     }
     printf("\n");
 }
 
-bool GetMaxHeightOfCommonBuildings (vector<int>* pVX, vector<int>* pVNextX)
-{
-    int maxHeight = 0;
-    vector<int>::iterator vxi = pVX->begin();
-    vector<int>::iterator vxe = pVX->end();
-    vector<int>::iterator vNextIter;
-    for(; vxi != vxe; ++vxi) {
-        vNextIter = find (pVNextX->begin(), pVNextX->end(), *vxi);
-        if (vNextIter != pVNextX->end()) {
-            if (maxHeight < buildingHeights[*vxi])
-                maxHeight = buildingHeights[*vxi];
-        }
+void PrintZoomedBuildings(int leftmost, int rightmost) {
+    int left = leftmost * 5;
+    int right = rightmost * 5;
+    printf("ZoomedLeft=%4d ZoomedRight=%4d\n", left, right);
+    for (int i = left; i <= right; ++i) {
+        printf("%4d", skyline[i][top]);
     }
-    return maxHeight;
+    printf("\n");
+    for (int i = left; i <= right; ++i) {
+        printf("%4d", skyline[i][bot]);
+    }
+    printf("\n");
+    for (int i = left; i <= right; ++i) {
+        printf("%4d", i);
+    }
+    printf("\n");
 }
 
-bool HaveCommonBuildings (vector<int>* pVX, vector<int>* pVNextX)
-{
-    vector<int>::iterator vxi = pVX->begin();
-    vector<int>::iterator vxe = pVX->end();
-    for(; vxi != vxe; ++vxi) {
-        if (find (pVNextX->begin(), pVNextX->end(), *vxi) != pVNextX->end()) {
-            return true;
-        }
-    }
-    return false;
-}
-
-void TraceSkyline(int leftmost, int rightmost)
-{
-    vector<int>* pVX = 0;
-    vector<int>* pVNextX = 0;
-    int x = leftmost - 1;
+/// The central idea is to store a zoomed in version of the building: 5 times zoomed in :)
+/// That will help in determining the common skyline connection between buildings which is
+/// otherwise difficult to find out :)
+void TraceSkyline(int leftmost, int rightmost) {
+    int zoomedLeft = leftmost * 5;
+    int zoomedRight = rightmost * 5;
+    int x = zoomedLeft;
     int nextX = 0;
-    bool shareCommonBuildings = false;
-    int maxHeightOfCommonBuildings = 0;
+    int prevX = 0;
 
-    while (x <= rightmost) {
-        if (skyline[x] == 0) ++x;
-        else break;
-    }
+    while (x <= zoomedRight) {
+        /// travel to the first non-zero height building
+        while (skyline[x][top] == 0) ++x;
 
-    if (x <= rightmost) {
-        printf("%d %d ", x, skyline[x]);
-        ++x;
-    }
+        //printf("Debug: %d %d\n", x, skyline[x][top]);
 
-    while (x <= rightmost) {
+        prevX = x - 1;
+        if (x <= zoomedRight) {
+            //printf("Debug: inside prevX %d %d\n", x, skyline[x][top]);
+            if (skyline[prevX][top] == 0)
+                printf ("%d %d ", x/5, skyline[x][top]);
+            //x += 1;
+        }
+
         nextX = x + 1;
-        if (nextX > rightmost) break;
-
-        pVX = &buildingIDs[x];
-        pVNextX = &buildingIDs[nextX];
-        shareCommonBuildings = HaveCommonBuildings(pVX, pVNextX);
-        maxHeightOfCommonBuildings = GetMaxHeightOfCommonBuildings(pVX, pVNextX);
-
-        if (skyline[x] == skyline[nextX]) {
-            if (shareCommonBuildings) {
-                printf("%d %d ", x, maxHeightOfCommonBuildings);
-                ++x;
-            }
-            else {
-                printf("%d %d ", x, 0);
-                ++x;
-            }
+        if (skyline[x][top] != skyline[nextX][top]) {
+            printf ("%d %d ", nextX/5, skyline[nextX][top]);
+            x += 2;
         }
         else {
-            if (shareCommonBuildings) {
-                printf("%d %d ", x, maxHeightOfCommonBuildings);
-                printf("%d %d ", nextX, skyline[nextX]);
-                x += 2;
-            }
-            else {
-                printf("%d %d ", x, 0);
-                ++x;
-            }
+            x += 1;
         }
-
-        if (skyline[x] == 0) {
-            while (x <= rightmost) {
-                if (skyline[x] == 0) ++x;
-                else break;
-            }
-            if (x <= rightmost) {
-                printf("%d %d ", x, skyline[x]);
-                ++x;
-            }
-        }
+        //printf("Debug: near while loop end: %d %d\n", x, skyline[x][top]);
     }
-    printf("%d\n", 0);
+
+    printf("\n");
 }
 
 int main()
 {
     int left = 0, height = 0, right = 0;
+    int zoomedInLeft = 0, zoomedInRight = 0;
     int leftmost = 0;
     int rightmost = 0;
-    int buildingID = 0;
+    int buildingNum = 0;
 
     while (scanf("%d %d %d", &left, &height, &right) != EOF) {
-        if (buildingID == 0) leftmost = left;
-        ++buildingID;
-
-        buildingHeights[buildingID] = height;
-
-        for (int i = left; i <= right; ++i) {
-            if (skyline[i] < height) {
-                skyline[i] = height;
+        if (buildingNum == 0) leftmost = left;
+        ++buildingNum;
+        zoomedInLeft  = left * 5;
+        zoomedInRight = right * 5;
+        for (int i = zoomedInLeft; i <= zoomedInRight; ++i) {
+            if (skyline[i][top] < height) {
+                skyline[i][bot] = skyline[i][top];
+                skyline[i][top] = height;
             }
-            buildingIDs[i].push_back(buildingID);
+            else {
+                if (skyline[i][bot] < height)
+                    skyline[i][bot] = height;
+            }
         }
     }
-
     rightmost = right + 1; // to find the end
-    PrintBuildings(leftmost, rightmost);
+    //PrintBuildings(leftmost, rightmost);
+    //PrintZoomedBuildings(leftmost, rightmost);
     TraceSkyline(leftmost, rightmost);
     return 0;
 }
